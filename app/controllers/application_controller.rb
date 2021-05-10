ca356a4e07079899a989f135b7b3626f7e9fa29e
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2021-05-11 00:47:05 +0200

    adding layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  layout 'application'
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || user_root_path
+  end
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name])
   end
 end
