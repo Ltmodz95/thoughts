23669487859112f2044299798def212433f24b36
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2021-05-20 17:03:42 +0200

    please

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Thoughts
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.serve_static_assets = true
+    config.assets.precompile = ['*.js', '*.css', '*.svg', '.*ttf']
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
